e1520813a0b3263b2ebe8852e54544bc922306c6
Monir11 <mounirrouissi2@gmail.com>
2024-03-05 07:03:40 +0100

    dev : Bifunctions

--- src/main/java/com/example/lesson1/Lesson1.java
+++ src/main/java/com/example/lesson1/Lesson1.java
@@ -1,5 +1,6 @@
 package com.example.lesson1;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Lesson1 {
@@ -13,6 +14,24 @@ public class Lesson1 {
         InnerLesson1 innerLesson1 = (c) -> "THREE" + c;
 
         System.out.println(innerLesson1.agePrint(5)); // THREE5
+
+
+
+// Bifunctions
+
+
+        BiFunction<Integer, Integer, Integer> sum = (a, b) -> a + b;
+
+        System.out.println(sum.apply(5, 10)); // 15
+
+
+        BiFunction<Integer, Integer, Integer> multiply = (a, b) -> a * b;
+        System.out.println(multiply.apply(5, 10));
+
+        BiFunction<Integer, Integer, Boolean> isAbiggerThanB = (a, b) -> a > b;
+        System.out.println(isAbiggerThanB.apply(50, 10));
+
+        
     }
 
 
